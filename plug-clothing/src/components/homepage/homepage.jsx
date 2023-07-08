db79b295f4ab7d95777fc470d587dda25f06fcf7
unknown <emmanueloguntolu48@gmail.com>
2023-07-08 17:57:27 +0100

    created project with create react app and added files from homepage

---
+++ plug-clothing/src/components/homepage/homepage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import '../homepage/homepage.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>BUCKET HATS</h1>
+                <div className='sub-title'>SHOP NOW</div>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>MENS</h1>
+                <div className='sub-title'>SHOP NOW</div>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>WOMAN</h1>
+                <div className='sub-title'>SHOP NOW</div>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>JACKETS</h1>
+                <div className='sub-title'>SHOP NOW</div>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>SNEAKERS</h1>
+                <div className='sub-title'>SHOP NOW</div>
+            </div>
+        </div>
+    </div>
+</div>
+)
+
+
+
+export default Homepage;
